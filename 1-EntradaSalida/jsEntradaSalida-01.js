47fbb4e958ebcc4c882ddd829e62d9a400ccc064
facundoayre <facu.ayre@gmail.com>
2021-07-07 18:38:14 -0300

    primer commit desde powershell

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("KEVO DAME UNA MANO POR FAVOR TE LO PIDO");
+	alert("me funciona powershell");
 }
 
