2e42b06196e78935b2946101e154fb090867fdc5
facundoayre <facu.ayre@gmail.com>
2021-07-08 00:06:18 -0300

    Primera clase CI programación

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,38 @@
 /*
+Facundo Ayre DIV-E
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+//asignación
+ //lo que está a la derecha se copia en lo que está a la izquierda
+
+
 function mostrar()
 {
-	alert("ok");
+	
+	
+ let nombre;
+ alert ("¿Cuál es su nombre?");
+ nombre = prompt ("Ingrese su nombre:");
+
+	
+	
+	alert ("Su nombre es: " +nombre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,20 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	//declaro la variable
+	let nombreIngresado;
+	//asigno la variable
+	//nombreIngresado=txtIdNombre.value;
+	//txtIdNombre
+
+//Esto se hace para que llame a algo que esta en el html
+	document.getElementById("txtIdNombre").value;
+//acá asignamos que eso que traemos es el contenido de la variable
+	nombreIngresado=document.getElementById("txtIdNombre").value;
+//con esto la publico
+	alert("Su nombre es: " +nombreIngresado);
+//esto es un añadido para que el cuadrado vuelva a estar en blanco
+	document.getElementById("txtIdNombre").value="";
 
 }
 
